81d9defa58c781496e7f234d6848bb5016f8539e
Magica <Stone23072004@gmail.com>
2024-11-30 20:35:01 +0700

    fixed BTH1

--- TH/B1/BTH1/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$username="MAGICA";
-$password="StoneMelon23";
-$database="flower";
-$conn=mysqli_connect($host,$username,$password,$database);
-mysqli_query($conn,"SET NAMES 'utf8'");
-?>
