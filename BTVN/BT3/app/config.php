45dd15759f33d4898b27175f69e5b7d9efe0b9bb
Magica <Stone23072004@gmail.com>
2024-12-03 21:34:43 +0700

    done MVC BT3

---
+++ BTVN/BT3/app/config.php
@@ -0,0 +1,2 @@
+<?php
+define('BASE_URL', 'http://localhost/Tlus_Music_Garden/CNW/BTVN/BT3/public/');
---
+++ BTVN/BT3/app/views/footer.php
@@ -0,0 +1,19 @@
+<div class="line"></div>
+<footer>
+    <p>TLU'S MUSIC GARDEN</p>
+</footer>
+<style>
+    .line {
+        height: 2px;
+        background-color: black;
+        margin: 50px 0 15px;
+    }
+    footer {
+        text-align: center;
+        color: black;
+        font-size: 35px;
+        font-weight: bold;
+    }
+</style>
+</body>
+</html>
