6c4c8b6ea50e2b38a85db8f222d83e8ebdffc15a
Magica <Stone23072004@gmail.com>
2024-12-04 10:47:05 +0700

    a bit animation

--- BTVN/BT3/app/views/footer.php
+++ BTVN/BT3/app/views/footer.php
@@ -1,6 +1,25 @@
-<div class="line"></div>
+
 <footer>
-    <p>TLU'S MUSIC GARDEN</p>
+    <h3>
+        <span class="char" style="--char-index: 0;">T</span>
+        <span class="char" style="--char-index: 1;">L</span>
+        <span class="char" style="--char-index: 2;">U</span>
+        <span class="char" style="--char-index: 3;">'</span>
+        <span class="char" style="--char-index: 4;">S</span>
+        <span class="char" style="--char-index: 5;"> </span>
+        <span class="char" style="--char-index: 6;">M</span>
+        <span class="char" style="--char-index: 7;">U</span>
+        <span class="char" style="--char-index: 8;">S</span>
+        <span class="char" style="--char-index: 9;">I</span>
+        <span class="char" style="--char-index: 10;">C</span>
+        <span class="char" style="--char-index: 11;"> </span>
+        <span class="char" style="--char-index: 12;">G</span>
+        <span class="char" style="--char-index: 13;">A</span>
+        <span class="char" style="--char-index: 14;">R</span>
+        <span class="char" style="--char-index: 15;">D</span>
+        <span class="char" style="--char-index: 16;">E</span>
+        <span class="char" style="--char-index: 17;">N</span>
+    </h3>
 </footer>
 <style>
     .line {
@@ -9,10 +28,38 @@
         margin: 50px 0 15px;
     }
     footer {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        border-top: 3px solid black;
         text-align: center;
         color: black;
         font-size: 35px;
         font-weight: bold;
+    }
+
+    footer h3 .char {
+        display: inline-block;
+        --delay:calc((var(--char-index) + 1) * 200ms);
+        animation: breathe 4000ms infinite both;
+        animation-delay: var(--delay);
+    }
+
+    @keyframes breathe {
+        0% {
+            opacity: 0;
+            transform: scale(0.9);
+        }
+
+        50% {
+            opacity: 1;
+            transform: scale(1);
+        }
+
+        100% {
+            opacity: 0;
+            transform: scale(0.9);
+        }
     }
 </style>
 </body>
