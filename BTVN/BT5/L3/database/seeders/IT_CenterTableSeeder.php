a3470c3d9dc25d3f09fb5122cd72bd8c2cb5d4d7
Magica <Stone23072004@gmail.com>
2024-12-11 22:10:29 +0700

    done L3 with hardware devices

--- BTVN/BT5/L3/database/seeders/IT_CenterTableSeeder.php
+++ BTVN/BT5/L3/database/seeders/IT_CenterTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
-class RentersTableSeeder extends Seeder
+class IT_CenterTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,12 +16,10 @@ class RentersTableSeeder extends Seeder
         $faker = Faker::create();
 
         for ($i = 0; $i < 20; $i++) {
-            DB::table('renters')->insert([
-                'name' => $faker->name(),
-                'phone_number' => $faker->phoneNumber(),
-                'email' => $faker->unique()->safeEmail(),
-                'created_at' => now(),
-                'updated_at' => now(),
+            DB::table('it_centers')->insert([
+                'name' => $faker->company(),
+                'location' => $faker->address(),
+                'contact_email' => $faker->unique()->safeEmail(),
             ]);
         }
     }
