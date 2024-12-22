51127a6d7a154b87dfcb3c27fd94512084e823f3
Magica <Stone23072004@gmail.com>
2024-12-22 23:58:47 +0700

    did try to fake some imgs but i failed

--- CSE485_MidTerm_01_2251172333/database/seeders/ProductsTableSeeder.php
+++ CSE485_MidTerm_01_2251172333/database/seeders/ProductsTableSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
-use Illuminate\Support\Facades\Storage;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -28,26 +27,12 @@ class ProductsTableSeeder extends Seeder
             'Power Bank',
         ];
 
-        // Tạo 10 ảnh ngẫu nhiên từ Faker và lưu vào thư mục storage/app/public/images
-        $images = [];
-        for ($i = 0; $i < 10; $i++) {
-            // Tạo tên ảnh ngẫu nhiên
-            $imageName = 'product_image_' . uniqid() . '.jpg';
-            // Đường dẫn lưu ảnh vào thư mục public/images
-            $imagePath = public_path('images/product_image_' . uniqid() . '.jpg');
-            $faker->image($imagePath, 640, 480, 'electronics', false);
-
-            // Thêm đường dẫn ảnh vào mảng
-            $images[] = 'images/' . $imageName;
-        }
-
-        // Thêm các bản ghi vào database
-        foreach (range(1, 100) as $index) {
+        foreach(range(1, 10) as $index) {
             DB::table('products')->insert([
                 'product_name' => $faker->randomElement($productNames),
                 'description' => $faker->sentence(),
                 'price' => $faker->numberBetween(10, 2000),
-                'image' => $faker->randomElement($images),
+                'image' => 'images/' . $faker->image('public/images', 640, 480, 'electronics', false),
                 'category_name' => $faker->randomElement(['Electronics', 'Accessories', 'Gaming', 'Home Appliances']),
             ]);
         }
