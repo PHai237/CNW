26dce53fec0f0e94b2cc690b5a8002ba4e69e031
Magica <Stone23072004@gmail.com>
2024-12-26 07:12:50 +0700

    changed name

---
+++ PhamHai_2251172333/database/migrations/2024_12_26_000752_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->enum('category', ['Mouse', 'Keyboard', 'Headset', 'Monitor']);
+            $table->text('description', 255);
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->string('supplier_email', 50)->unique();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
